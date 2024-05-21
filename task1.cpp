3be8ce6e971324353d3063811a8293f8a9c34c92
Error <egor.gmag@yandex.ru>
2024-05-21 16:48:45 +0000

    lab3 added

---
+++ task1.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <mutex>
+#include <thread>
+#include <chrono>
+#include <vector>
+#define COUNT 20000
+
+using namespace std;
+std::mutex mut;
+
+double* x; // вектор, на который умножается матрица
+double* M; // матрица
+double* res; //результат умножения
+
+void define(size_t left, size_t right) {
+    for (int i = left; i < right; i++) { // Инициализируем вектор x значениями 1 и массив res значениями 0.
+        x[i] = 1;
+        res[i] = 0;
+    }
+
+    for (int i = left * COUNT; i < right * COUNT; i++) { // Инициализируем матрицу M значениями 5.
+        M[i] = 5;
+    }
+}
+
+void matrixMult(size_t left, size_t right) { // Умножаем матрицу M на вектор x и сохраняем результат в массиве res.
+    int half_sum = 0;
+
+    for (int i = left; i < right; i++) { // определяем диапазон элементов матрицы, который будет обрабатываться текущим потоком.
+        half_sum += M[i] * x[i % COUNT];
+
+        if (i % COUNT == COUNT - 1) {// Когда индекс i достигает конца строки матрицы, half_sum добавляется к элементу вектора с использованием мьютекса для защиты разделяемого ресурса.
+            mut.lock();
+            res[i / COUNT] += half_sum;
+            half_sum = 0;
+            mut.unlock();
+        }
+    }
+
+    if ((right - 1) % COUNT == COUNT - 1) { // Если последний элемент в диапазоне также завершает строку матрицы, остаточная частичная сумма добавляется к соответствующему элементу результирующего вектора.
+        mut.lock();
+        res[(right - 1)/ COUNT] += half_sum;
+        mut.unlock();
+    }
+}
+
+int main()
+{
+    x = new double[COUNT];
+    res = new double[COUNT];
+    M = new double[COUNT*COUNT];
+
+    for (int j = 1; j <= 40; j++) { // создаем необходимое количество потоков для инициализации и умножения.
+        int countThreads = j, left, right, items_per_thread = COUNT / countThreads, items_per_thread_mod = COUNT % countThreads;
+
+        vector<thread> threads;
+
+        auto start = std::chrono::steady_clock::now();
+        for (int i = 0; i < countThreads; i++) {
+            left = i * items_per_thread + (items_per_thread_mod > i? i: items_per_thread_mod);
+            right = (i + 1) * items_per_thread + (items_per_thread_mod > i? i + 1: items_per_thread_mod);
+
+            threads.push_back(thread(define, left, right));
+        }
+
+        for (auto &thread : threads) {
+            thread.join();
+        }
+
+        threads.clear();
+        items_per_thread = (COUNT * COUNT) / countThreads, items_per_thread_mod = (COUNT * COUNT) % countThreads;
+
+        for (int i = 0; i < countThreads; i++) {
+            left = i * items_per_thread + (items_per_thread_mod > i? i: items_per_thread_mod);
+            right = (i + 1) * items_per_thread + (items_per_thread_mod > i? i + 1: items_per_thread_mod);
+            threads.push_back(thread(matrixMult, left, right));
+        }
+
+        for (auto &thread : threads) {
+            thread.join();
+        }
+
+        auto end = std::chrono::steady_clock::now();
+
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+
+        std::cout << "Time taken for step " << j << ": " << duration.count() << " milliseconds." << std::endl;
+
+        threads.clear();
+    }
+
+    for (int i = 0; i < COUNT; i++) { // Вычисляем время выполнения каждого шага и выводит его на экран.
+        cout << res[i] << ' ';
+    }
+
+    return 0;
+}
---
+++ task2.cpp
@@ -0,0 +1,226 @@
+#include <iostream>
+#include <future>
+#include <thread>
+#include <math.h>
+#include <queue>
+#include <unordered_map>
+
+#define Type double
+
+using namespace std;
+
+condition_variable condVar, condVar2;
+
+mutex mut;
+
+queue<pair<size_t, future<Type>>> tasks;
+queue<pair<size_t, future<Type>>> get_tasks;
+
+unordered_map<int, Type> results;
+
+void server_thread(const stop_token& stoken)
+{
+    unique_lock lock_res{mut, defer_lock};
+    size_t id_task;
+    future<Type> task;
+
+    while (!stoken.stop_requested()) // Пока не запрошено завершение работы сервера, поток обрабатывает задачи.
+    {
+        lock_res.lock(); 
+
+        if (tasks.empty()) { // Если очередь пуста, сервер ожидает сигнала от условной переменной condVar, чтобы продолжить выполнение.
+            condVar.wait_for(lock_res, chrono::seconds(1s));
+        }
+
+        if (!tasks.empty()) { // Если очередь задач не пуста, первая задача перемещается в очередь
+            get_tasks.push({tasks.front().first, std::move(tasks.front().second)});
+
+            tasks.pop();
+        }
+
+        lock_res.unlock();
+
+        while (!get_tasks.empty()) { // Обрабатываются все задачи в get_tasks: результаты выполнения задач сохраняются в контейнер results, и условная переменная condVar2 уведомляет клиентов о готовности результатов.
+            id_task = get_tasks.front().first;
+            Type result = get_tasks.front().second.get();
+
+            lock_res.lock();
+
+            results.insert({id_task, result});
+
+            get_tasks.pop();
+
+            condVar2.notify_all();
+
+            lock_res.unlock();
+        }
+    }
+
+    cout << "Server stop!\n";
+}
+
+template<typename T>
+class Server{
+public:
+    void start() {
+        cout << "Start\n";
+        server = jthread(server_thread);
+    };
+
+    void stop() {
+        server.request_stop();
+        server.join();
+
+        cout << "End\n";
+    };
+
+    size_t add_task(future<T> task) { // добавляет задачу в очередь задач, присваивая ей уникальный идентификатор
+        size_t id = rand;
+        tasks.push({id, std::move(task)});
+        rand++;
+
+        return id;
+    };
+
+    T request_result(size_t id_res) { // возвращает результат выполнения задачи по её идентификатору и удаляет её из контейнера результатов
+        T res = results[id_res];
+
+        results.erase(id_res);
+
+        return res;
+    };
+
+private:
+    size_t rand = 0;
+    jthread server;
+};
+
+Server<Type> server;
+
+template<typename T>
+T fun_sin(T arg) {
+    return sin(arg);
+}
+
+template<typename T>
+T fun_sqrt(T arg) {
+    return sqrt(arg);
+}
+
+template<typename T>
+T fun_pow(T x, T y) {
+    return pow(x, y);
+}
+
+template<typename T>
+void add_task_1() {
+    unique_lock lock_res(mut, defer_lock);
+
+    for (int i = 0; i < 10000; i++) {
+        future<T> result = async(launch::deferred, [](){return fun_sin<T>(numbers::pi/2);});
+
+        lock_res.lock();
+
+        size_t id = server.add_task(std::move(result)); // Клиент создаёт задачу на вычисление синуса и добавляет её на сервер
+
+        condVar.notify_one();
+
+        lock_res.unlock();
+
+        lock_res.lock();
+
+        while (results.find(id) == results.end()) {
+            condVar2.wait(lock_res); // Клиент ожидает завершения задачи и получения результата
+
+            if (results.find(id) != results.end()) {
+                cout << "task_thread result:\t" << server.request_result(id) << endl;
+
+                break;
+            }
+        }
+
+        lock_res.unlock();
+    }
+
+    cout << "Client 1 work is done" << endl;
+}
+
+template<typename T>
+void add_task_2() {
+    unique_lock lock_res(mut, defer_lock);
+
+    for (int i = 0; i < 10000; i++) {
+        future<T> result = async(launch::deferred, [](){return fun_sqrt<T>(4);});
+
+        lock_res.lock();
+
+        size_t id = server.add_task(std::move(result));
+
+        condVar.notify_one();
+
+        lock_res.unlock();
+
+        lock_res.lock();
+
+        while (results.find(id) == results.end()) {
+            condVar2.wait(lock_res);
+
+            if (results.find(id) != results.end()) {
+                cout << "task_thread result:\t" << server.request_result(id) << endl;
+
+                break;
+            }
+        }
+
+        lock_res.unlock();
+    }
+
+    cout << "Client 2 work is done" << endl;
+}
+
+template<typename T>
+void add_task_3() {
+    unique_lock lock_res(mut, defer_lock);
+
+    for (int i = 0; i < 10000; i++) {
+        future<T> result = async(launch::deferred, [](){return fun_pow<T>(2, 3);});
+
+        lock_res.lock();
+
+        size_t id = server.add_task(std::move(result));
+
+        condVar.notify_one();
+
+        lock_res.unlock();
+
+        lock_res.lock();
+
+        while (results.find(id) == results.end()) {
+            condVar2.wait(lock_res);
+
+            if (results.find(id) != results.end()) {
+                cout << "task_thread result:\t" << server.request_result(id) << endl;
+
+                break;
+            }
+        }
+
+        lock_res.unlock();
+    }
+
+    cout << "Client 3 work is done" << endl;
+}
+
+int main() {
+    server.start();
+
+    thread task_1(add_task_1<Type>);
+    thread task_2(add_task_2<Type>);
+    thread task_3(add_task_3<Type>);
+
+    task_1.join();
+    task_2.join();
+    task_3.join();
+
+    server.stop();
+}
